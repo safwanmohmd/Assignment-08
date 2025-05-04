5ed16542702b46d70685ca5a29b73f64ce2fd46d
safwan <safwankilinakode53@gmail.com>
2025-05-04 21:28:26 +0530

    btn disable added

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 let taskInp = document.getElementById("inp");
 let submit = document.getElementById("form");
 let list = document.getElementById("list");
-let categories = document.getElementById("categories")
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 let filterTask = [...tasks]
 let listitem = document.getElementById("listitem")
 
+
+
+
+//btn
+
+let btngroup = document.querySelectorAll(".btn-dark")
+
+
+filterTask = tasks;
 display()
-categories.addEventListener("change", () => {
 
+btngroup.forEach((exe) => {
 
-    filterTask = tasks.filter((task) => {
+    exe.addEventListener('click', () => {
+        filterTask = tasks.filter((task) => {
+            if (exe.innerText == 'All') {
+                return task
+            } else {
+                return task.status == exe.innerText
+            }
 
-        return task.status == categories.value || categories.value == ""
+        })
+        console.log(filterTask);
+        display();
     })
-    display();
 })
 
 form.addEventListener("submit", (e) => {
@@ -44,31 +59,31 @@ function display() {
         </ul>`;
 
         list.appendChild(card);
-      
-        if (taskss.status == "Completed"){
-       
+        let completeBtn = card.querySelector("#completeBtn");
+        if (taskss.status == "Completed") {
+
             card.classList.add('completed')
-            
+              completeBtn.innerText = 'COMPLETED'
+              completeBtn.disabled = true
         }
         let deleteBtn = card.querySelector("#dltBtn");
         deleteBtn.addEventListener("click", () => {
-            
+
             card.remove();
         });
 
-        let completeBtn = card.querySelector("#completeBtn");
+        
         completeBtn.addEventListener("click", () => {
-            if (taskss.status == "Completed"){
+            if (taskss.status == "Completed") {
                 alert("the task alredy completed")
                 return;
             }
             taskss.status = "Completed"
             console.log(taskss.status);
-            if (categories.value !== '') {
-                card.remove(); 
-            }
+                card.remove();
             
-           
+
+
             localStorage.setItem("tasks", JSON.stringify(tasks));
 
         });
